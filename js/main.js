ad17ba61a66058101cf807b347fd58faa31acc40
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2022-10-13 19:02:20 +0300

    add position elements

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const containerNode = document.getElementById('game');
+const itemNodes = Array.from(containerNode.querySelectorAll('.game__item'));
+const countItems = 16;
+
+if(itemNodes.length !== 16) {
+    throw new Error(`should be exactly ${countItems} items in HTML`)
+}
+
+// Position
+itemNodes[countItems - 1].style.display = 'none';
+let matrix = getMatrix(
+    itemNodes.map(item => Number(item.dataset.matrixId))
+)
+setPositionItems(matrix);
+
+//  shufl
+
+
+
+
+// Helpers
+function getMatrix(arr) {
+    const matrix = [[], [], [], []];
+    let y = 0;
+    let x = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(x >= 4) {
+            y++;
+            x = 0;
+        }
+        matrix[y][x] = arr[i];
+        x++;
+    }
+
+    return matrix;
+}
+
+function setPositionItems(matrix) {
+    for(let y = 0; y < matrix.length; y++) {
+        for(let x = 0; x < matrix[y].length; x++) {
+            const value = matrix[y][x];
+            const node = itemNodes[value - 1];
+            setNodeStyles(node, x, y);
+        }
+    }
+}
+
+function setNodeStyles(node, x, y) {
+    const shiftPs = 100;
+    node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`;
+}
